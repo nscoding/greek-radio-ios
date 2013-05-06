3ca2b68fc3005e5f342c5dd7bdd15441cafd3c33
nscoding <patrick@nscoding.co.uk>
2013-05-06 20:19:45 +0200

    Code refactoring

--- Source/Greek Radio/Greek Radio/Views/Cells/GRStationCellView.m
+++ Source/Greek Radio/Greek Radio/Views/Cells/GRStationCellView.m
@@ -42,7 +42,11 @@
         self.title.textAlignment = NSTextAlignmentLeft;
         self.title.numberOfLines = 1;
 
-        self.subtitle.textColor = [UIColor colorWithRed:0.153f green:0.075f blue:0.024f alpha:1.00f];
+        self.subtitle.textColor = [UIColor colorWithRed:0.153f
+                                                  green:0.075f
+                                                   blue:0.024f
+                                                  alpha:1.00f];
+        
         self.subtitle.shadowColor = [UIColor colorWithWhite:0.9 alpha:1.0];
         self.subtitle.shadowOffset = CGSizeMake(0, 1);
         self.subtitle.textAlignment = NSTextAlignmentLeft;
