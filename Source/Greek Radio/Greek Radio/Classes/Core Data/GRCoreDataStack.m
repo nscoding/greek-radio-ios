2dc90081d9846b1b6adc0769894e95041f6a6871
nscoding <patrick@nscoding.co.uk>
2013-06-06 18:07:24 +0200

    iCloud support

--- Source/Greek Radio/Greek Radio/Classes/Core Data/GRCoreDataStack.m
+++ Source/Greek Radio/Greek Radio/Classes/Core Data/GRCoreDataStack.m
@@ -133,6 +133,7 @@
     return managedObjectContext;
 }
 
+
 /**
  Returns the managed object model for the application.
  If the model doesn't already exist, it is created from the application's model.
@@ -152,12 +153,111 @@
     return _managedObjectModel;
 }
 
+
 /**
  Returns the persistent store coordinator for the application.
  If the coordinator doesn't already exist, it is created and the application's store added to it.
  */
 - (NSPersistentStoreCoordinator *)persistentStoreCoordinator
 {
+    if((_persistentStoreCoordinator != nil)) {
+        return _persistentStoreCoordinator;
+    }
+    
+    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc]
+                                   initWithManagedObjectModel:[self managedObjectModel]];
+    NSPersistentStoreCoordinator *psc = _persistentStoreCoordinator;
+    
+    // Set up iCloud in another thread:
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        // ** Note: if you adapt this code for your own use, you MUST change this variable:
+        NSString *iCloudEnabledAppID = @"2HJK7966XV.com.NSCoding.GreekRadio";
+        
+        // ** Note: if you adapt this code for your own use, you should change this variable:
+        NSString *dataFileName = @"GreekRadioModel.sqlite";
+        
+        // ** Note: For basic usage you shouldn't need to change anything else
+        
+        NSString *iCloudDataDirectoryName = @"Data.nosync";
+        NSString *iCloudLogsDirectoryName = @"Logs";
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        
+        NSURL *localStore = [[self applicationDocumentsDirectoryURL]
+                             URLByAppendingPathComponent:@"GreekRadioModel.sqlite"];
+        NSURL *iCloud = [fileManager URLForUbiquityContainerIdentifier:nil];
+        
+        if (iCloud) {
+            
+            NSLog(@"iCloud is working");
+            
+            NSURL *iCloudLogsPath = [NSURL fileURLWithPath:[[iCloud path]
+                                                            stringByAppendingPathComponent:iCloudLogsDirectoryName]];
+            
+            NSLog(@"iCloudEnabledAppID = %@",iCloudEnabledAppID);
+            NSLog(@"dataFileName = %@", dataFileName);
+            NSLog(@"iCloudDataDirectoryName = %@", iCloudDataDirectoryName);
+            NSLog(@"iCloudLogsDirectoryName = %@", iCloudLogsDirectoryName);
+            NSLog(@"iCloud = %@", iCloud);
+            NSLog(@"iCloudLogsPath = %@", iCloudLogsPath);
+            
+            if([fileManager fileExistsAtPath:[[iCloud path] stringByAppendingPathComponent:iCloudDataDirectoryName]] == NO) {
+                NSError *fileSystemError;
+                [fileManager createDirectoryAtPath:[[iCloud path] stringByAppendingPathComponent:iCloudDataDirectoryName]
+                       withIntermediateDirectories:YES
+                                        attributes:nil
+                                             error:&fileSystemError];
+                if(fileSystemError != nil) {
+                    NSLog(@"Error creating database directory %@", fileSystemError);
+                }
+            }
+            
+            NSString *iCloudData = [[[iCloud path]
+                                     stringByAppendingPathComponent:iCloudDataDirectoryName]
+                                    stringByAppendingPathComponent:dataFileName];
+            
+            NSLog(@"iCloudData = %@", iCloudData);
+            
+            NSMutableDictionary *options = [NSMutableDictionary dictionary];
+            [options setObject:[NSNumber numberWithBool:YES] forKey:NSMigratePersistentStoresAutomaticallyOption];
+            [options setObject:[NSNumber numberWithBool:YES] forKey:NSInferMappingModelAutomaticallyOption];
+            [options setObject:iCloudEnabledAppID            forKey:NSPersistentStoreUbiquitousContentNameKey];
+            [options setObject:iCloudLogsPath                forKey:NSPersistentStoreUbiquitousContentURLKey];
+            
+            [psc lock];
+            
+            [psc addPersistentStoreWithType:NSSQLiteStoreType
+                              configuration:nil
+                                        URL:[NSURL fileURLWithPath:iCloudData]
+                                    options:options
+                                      error:nil];
+            
+            [psc unlock];
+        }
+        else {
+            NSLog(@"iCloud is NOT working - using a local store");
+            NSMutableDictionary *options = [NSMutableDictionary dictionary];
+            [options setObject:[NSNumber numberWithBool:YES] forKey:NSMigratePersistentStoresAutomaticallyOption];
+            [options setObject:[NSNumber numberWithBool:YES] forKey:NSInferMappingModelAutomaticallyOption];
+            
+            [psc lock];
+            
+            [psc addPersistentStoreWithType:NSSQLiteStoreType
+                              configuration:nil
+                                        URL:localStore
+                                    options:options
+                                      error:nil];
+            [psc unlock];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"SomethingChanged" object:self userInfo:nil];
+        });
+    });
+    
+    return _persistentStoreCoordinator;
+
+/*
     if (_persistentStoreCoordinator != nil)
     {
         return _persistentStoreCoordinator;
@@ -200,6 +300,7 @@
     }
     
     return _persistentStoreCoordinator;
+*/
 }
 
 
@@ -209,7 +310,8 @@
 // ------------------------------------------------------------------------------------------
 - (NSArray *)fetchObjectsForEntityName:(NSString *)newEntityName withPredicate:(NSPredicate*)predicate
 {
-    NSEntityDescription *entity = [NSEntityDescription entityForName:newEntityName inManagedObjectContext:[self managedObjectContext]];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:newEntityName
+                                              inManagedObjectContext:[self managedObjectContext]];
 	
     NSFetchRequest *request = [[NSFetchRequest alloc] init];
     request.entity = entity;
